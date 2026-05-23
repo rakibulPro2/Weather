1c5ccfc6368f176d099d6ba7d92394772ad3bad4
rakibulPro2 <info.rakib33@gmail.com>
2026-05-23 05:05:56 -0700

    weather prject with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[100%] h-[100vh] bg-emerald-500">
+      <div className="w-[1320px] mx-auto text-center">
+        <div>
+          <h1 className="text-white font-bold text-[30px] py-5 text-center">
+            Simple Weather Check For UAE
+          </h1>
+          <form>
+            <input
+              className="w-[520px] px-3 py-4 rounded-sm shadow-lg"
+              type="text"
+              placeholder="Check Weather for city"
+            />
+            <button className="px-5 py-4 bg-white ml-4  rounded-sm shadow-lg">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="w-[400px] bg-white mx-auto shadow-lg mt-[40px] p-[25px]">
+          <h3 className="font-bold text-[30px]">
+            Abu Dhabi <span className="bg-[yellow]">In</span>
+          </h3>
+          <h2 className="font-bold text-[40px]">9.62oc</h2>
+          <img src=""></img>
+          <p>Fog</p>
+        </div>
+      </div>
     </div>
   );
 }
